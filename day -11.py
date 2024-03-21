84f65ecb3632a0d9df6a2c820f0b9dce739f0f58
Devihari123 <162981663+Devihari123@users.noreply.github.com>
2024-03-21 11:45:09 +0530

    Add files via upload

---
+++ day -11.py
@@ -0,0 +1,70 @@
+#print("hello world")
+'''
+print("hello world")
+print("hello world")
+print("hello world")
+'''
+"""
+print("hello world")
+print("helo world")
+print("hello world")
+print("helo world")
+"""
+'''
+NOTE--->
+1.commenting is used for making decumentation to in a python file.
+2.when the line is commeted,that line will not be considered as a part of the code.
+3.when the program is executed ,commented line will not be in the part of execution.
+NOTE:
+1.single line commenting can be done by using # at the begininng of the line.
+eg:#print("hello world")
+2.Multiple line commenting can be done by using ''' code ''' the lines between the quotes will be commented.
+eg:'''
+code
+''''
+eg:
+"""
+code
+"""
+'''
+import this
+print(this)
+'''
+'''
+import is a keyword which is used to communicate between the python files.
+'''
+BARGAINED CHARACTERS:
+1.CONTROLS AND DATA STRUCTURES --->ABC LANGUAGE
+2.STRING &ITS FORMATE--->FROM C
+3.REGULAR EXPRESSIONS--->PERL
+4.EXCEPTIONAL HANDLING --->MODULE 3
+5.MAGIC METHODS FROM--->FORTRON
+'''
+'''
+print("hello")
+print("hello")
+import this
+print(this)
+import antigravity
+'''
+"""
+WHAT KIND OF LANGUAGE IS PYTHON:
+  1.object directed programing language.
+  2.dynamically typed language.
+  3.interpreted language.
+  4.compiled language.
+  """
+'''
+VARIABLES:
+1.variable is a name which is used to identify the value of variable.
+2.while is stored in a specific memory location.
+RULES TO DEFINE VARIABLES :
+1.variable have to star with only alphabets
+unserscore.
+2.the char following the 1st char can be alphabets,numbers,underscore.
+3.no spaces are allowed.
+4.no special character like !@$%^&
+5.case sensitive
+6.keywords are not allowed as variables.
+
+
